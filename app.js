6cc0e0a1aeedba6791c25bc028ec9571b9f2bff5
22012497-Jethro <138637175+22012497-Jethro@users.noreply.github.com>
2024-07-05 17:04:14 +0800

    Changed the main page

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
