8b52e931f96ffdef5bcd701d96818b52dd188c24
JETHRO TAI QI XUAN <22012497@myrp.edu.sg>
2024-07-11 14:29:52 +0800

    IDK

--- supabase_connection.py
+++ supabase_connection.py
@@ -1,7 +1,7 @@
 from supabase import create_client, Client
 
 # Replace these with your Supabase URL and API Key
-url: str = "https://fudsrzbhqpmryvmxgced.supabase.co/rest/v1/"
+url: str = "https://fudsrzbhqpmryvmxgced.supabase.co"
 key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1ZHNyemJocXBtcnl2bXhnY2VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM5MjE3OTQsImV4cCI6MjAyOTQ5Nzc5NH0.6UMbzoD8J1BQl01h6NSyZAHVhrWerUcD5VVGuBwRcag"
 
 # Create a Supabase client
