fd834bd4a20a0690ffa336d271398eaca3ae9821
JETHRO TAI QI XUAN <22012497@myrp.edu.sg>
2024-07-11 13:56:52 +0800

    Updated userProfile.js

--- public/userProfile.js
+++ public/userProfile.js
@@ -5,8 +5,10 @@ async function fetchAndDisplayUserData() {
             throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        document.getElementById('profile-username').textContent = data.username;
-        document.getElementById('profile-pic').src = data.pfp || 'default-profile.png';
+        if (data) {
+            document.getElementById('profile-username').textContent = data.username;
+            document.getElementById('profile-pic').src = data.pfp || 'default-profile.png';
+        }
     } catch (error) {
         console.error('Error fetching user data:', error);
     }
