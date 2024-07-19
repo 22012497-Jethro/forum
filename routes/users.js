c06db040faba432ece10ed1db9f8724c9b69739d
JETHRO TAI QI XUAN <22012497@myrp.edu.sg>
2024-07-20 02:12:38 +0800

    signup try #4

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require('bcrypt');
 const { createClient } = require('@supabase/supabase-js');
 
 const router = express.Router();
@@ -8,7 +9,6 @@ const supabaseUrl = 'https://fudsrzbhqpmryvmxgced.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1ZHNyemJocXBtcnl2bXhnY2VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM5MjE3OTQsImV4cCI6MjAyOTQ5Nzc5NH0.6UMbzoD8J1BQl01h6NSyZAHVhrWerUcD5VVGuBwRcag';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-// Signup route
 // Signup route
 router.post('/signup', async (req, res) => {
     const { username, email, password, confirmPassword } = req.body;
