4df909e5030a7ae0dd16365bd9d826c7d2c03b51
JETHRO TAI QI XUAN <22012497@myrp.edu.sg>
2024-07-22 18:25:31 +0800

    comments try #2

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,15 @@
-const express = require('express');
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const { createClient } = require("@supabase/supabase-js");
+
 const router = express.Router();
-const { supabase } = require('../supabaseClient');
+
+// Supabase setup
+const supabaseUrl = "https://fudsrzbhqpmryvmxgced.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1ZHNyemJocXBtcnl2bXhnY2VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM5MjE3OTQsImV4cCI6MjAyOTQ5Nzc5NH0.6UMbzoD8J1BQl01h6NSyZAHVhrWerUcD5VVGuBwRcag";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Get comments for a post
 router.get('/:postId', async (req, res) => {
