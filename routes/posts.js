d6052139bb2055518bcb2f5f0b68f0fd9c153acf
JETHRO TAI QI XUAN <22012497@myrp.edu.sg>
2024-07-22 15:44:32 +0800

    edit try #2

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,6 @@ const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 const { createClient } = require("@supabase/supabase-js");
-const { deleteImage, uploadImage } = require('../helpers');
 
 const router = express.Router();
 
