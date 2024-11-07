69462e163855d90386517f55ebb0dc0b18ff6d6e
22012497-Jethro <138637175+22012497-Jethro@users.noreply.github.com>
2024-11-07 14:01:32 +0800

    Update main.js

--- public/main.js
+++ public/main.js
@@ -23,7 +23,11 @@ function loadPosts() {
                 const postElement = document.createElement('div');
                 postElement.classList.add('post');
                 postElement.setAttribute('data-post-id', post.id);
-                postElement.innerHTML = `
+                ppostElement.innerHTML = `
+                    <div class="post-header">
+                        <a href="/profile/${post.user_id}" class="post-username">${post.username}</a>
+                        <img src="${post.profile_pic || 'default-profile.png'}" alt="${post.username}'s profile picture" class="creator-pfp">
+                    </div>
                     <h4 class="post-title"><a href="/single-post.html?post_id=${post.id}">${post.title}</a></h4>
                     <p class="post-content">${post.caption}</p>
                     <img src="${post.image}" alt="${post.title}" />
